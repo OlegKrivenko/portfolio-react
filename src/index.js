b6de8584204a41e22f89514a62988e266245576e
OlegKrivenko <krivenko.olegus@gmail.com>
2025-09-16 19:22:09 +0200

    add in index,js basename="/portfolio-react"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import ScrollToTop from "./utils/scrollToTop";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import ScrollToTop from './utils/scrollToTop';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-react">
       {/* для того чтобы новая страница по навигации окрывалась всегда с самого начала (с самого верха)
       взято инфу с https://v5.reactrouter.com/web/guides/scroll-restoration
       */}
