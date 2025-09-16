b6327b00d431d8f2d1e2f0d6c9367396296d9f54
OlegKrivenko <krivenko.olegus@gmail.com>
2025-09-16 21:09:17 +0200

    add  component ButtonDarkMode

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,6 @@
-import "./style.css";
-
-import sun from "../../img/icons/sun.svg";
-import moon from "../../img/icons/moon.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+import ButtonDarkMode from 'components/ButtonDarkMode/ButtonDarkMode';
+import './style.css';
 
 const Navbar = () => {
   return (
@@ -13,10 +11,7 @@ const Navbar = () => {
             Portfolio
           </NavLink>
 
-          <button className="dark-mode-btn">
-            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-          </button>
+          <ButtonDarkMode />
 
           <ul className="nav-list">
             <li className="nav-list__item">
